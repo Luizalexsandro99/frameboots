268a9cb6547df7dc1d83ee2a35ecdcfe3a97d674
Luiz Alexsandro <100519613+Luizalexsandro99@users.noreply.github.com>
2022-10-07 11:30:01 -0300

    header

---
+++ script/scripts.js
@@ -0,0 +1,9 @@
+divImg.onmouseenter = function enterimage(){
+    paragConhe.style.display = 'block'
+    navDocumentation.classList.add('documentationcolorclass')
+}
+
+divImg.onmouseout = function enterimage(){
+    paragConhe.style.display = 'none'
+    navDocumentation.classList.remove('documentationcolorclass')
+}
---
+++ script/variable.js
@@ -0,0 +1,3 @@
+const divImg = document.querySelector('#divImg')
+const paragConhe = document.querySelector('#paragConhe')
+const navDocumentation = document.querySelector('#navDocumentation')
